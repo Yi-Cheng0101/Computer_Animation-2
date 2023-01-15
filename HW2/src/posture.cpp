b8d05f5dbfa9438535d6ec0eecb5aded5d80d781
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2023-01-15 23:58:32 +0800

    Delete HW2/src directory

--- HW2/src/posture.cpp
+++
@@ -1,6 +0,0 @@
-#include "posture.h"
-
-#include <utility>
-
-Posture::Posture(const std::size_t size) noexcept :
-    rotations(size, Eigen::Quaternionf::Identity()), translations(size, Eigen::Vector3f::Zero()) {}
